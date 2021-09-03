98adb2c9bcf0637dcdf08da2897e4d546207b4f0
Hakan Yiğit <khan.yigit@gmail.com>
2021-09-03 19:27:53 +0300

    f

---
+++ EfaturaFinalHandler/Models/documentResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace EfaturaFinalHandler.Models 
+{
+    [DataContract]
+    public class documentReturnType
+    {
+        [DataMember]
+        public string msg { get; set; }
+        [DataMember]
+        public string hash { get; set; }
+    }
+}
