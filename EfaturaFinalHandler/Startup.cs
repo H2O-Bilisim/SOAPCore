d2bd66dfe975cdb61b0aa6c5259db75fbef34c40
Orkun Gürler <orkungurler@hotmail.com>
2021-09-20 00:05:08 +0300

    change

--- EfaturaFinalHandler/Startup.cs
+++ EfaturaFinalHandler/Startup.cs
@@ -76,9 +76,9 @@ namespace EfaturaFinalHandler
                     context.Request.Body = initialBody;
                 }
             });
-            app.UseSoapEndpoint<IFaturaService>(path: "/gibhandler.wsdl", binding: new BasicHttpBinding());
-            app.UseSoapEndpoint<IFaturaService>(path: "/gibhandler", binding: new BasicHttpBinding());
-
+            app.UseSoapEndpoint<EFatura>("/gibhandler.wsdl", new SoapEncoderOptions());
+            app.UseSoapEndpoint<EFatura>("/", new SoapEncoderOptions());
+            
 
         }
     }
